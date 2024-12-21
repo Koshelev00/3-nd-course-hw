08d4508d3004c110d460fdd430a1419394e78e05
Koshelev Aleksey <koschelev00@mail.ru>
2024-12-21 18:46:12 +0300

    сделал HW-4

---
+++ modules/createCommentObject.js
@@ -0,0 +1,10 @@
+export const createCommentObject = (name, text, likes, liked) => {
+    let now = moment().format('D.MM.YY HH:mm')
+    return {
+        name: name,
+        text: text,
+        likes: likes || 0,
+        liked: liked || false,
+        dates: now,
+    }
+}
