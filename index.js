363fe0ad970f92e1050e82b5517d29a0c31e2357
Koshelev Aleksey <koschelev00@mail.ru>
2024-12-21 19:08:17 +0300

    HW-3

--- index.js
+++
@@ -1,9 +0,0 @@
-import { comments } from './modules/comments.js'
-import { renderComment } from './modules/renderComment.js'
-
-import { addComment } from './modules/listener.js'
-renderComment(comments)
-
-const addButton = document.getElementById('add-form-button')
-
-addButton.addEventListener('click', addComment)
